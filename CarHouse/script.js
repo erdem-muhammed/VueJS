33b7b40a82b0614c57773fc080220b601134de24
muhammed erdem <erdem.muhammed1@outlook.com>
2021-09-22 22:53:53 +0200

    vue data and click function added

---
+++ CarHouse/script.js
@@ -0,0 +1,22 @@
+
+
+
+const app = new Vue(
+    {
+        el: "#app",
+        data:
+        {
+            cars: [],
+            in_titel: "",
+            in_subtitel: "",
+            in_fuel: "",
+            in_enginePower: "",
+            in_kilometer: "",
+            in_year: "",
+        },
+        methods:
+        {
+            
+        }
+    }
+)
