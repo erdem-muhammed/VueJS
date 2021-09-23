2b595cb1a168f858ba7dcc62e5286a1870f5fd2d
muhammed erdem <erdem.muhammed1@outlook.com>
2021-09-23 22:00:33 +0200

    .card adjusted to VueJS

---
+++ ShoppingPage with VueJS/script.js
@@ -0,0 +1,41 @@
+const app = new Vue(
+    {
+        el: "#app",
+        data:
+        {
+            products:
+            [
+                {
+                    titel: "iPhone 5",
+                    image: "iphone.jpg",
+                    price: 1450,
+                },
+                {
+                    titel: "Galaxy S9",
+                    image: "iphone.jpg",
+                    price: 300
+                },
+                {
+                    titel: "Galaxy S7",
+                    image: "iphone.jpg",
+                    price: 200.15
+                },
+                {
+                    titel: "iPhone 8",
+                    image: "iphone.jpg",
+                    price: 800
+                },
+                {
+                    titel: "Galaxy S8",
+                    image: "iphone.jpg",
+                    price: 400
+                },
+                {
+                    titel: "Galaxy S6",
+                    image: "iphone.jpg",
+                    price: 200
+                }
+            ]
+        }
+    }
+)
