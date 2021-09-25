3e13c2bb2c5cb8cd711dbb0ae3460c32d482f4fd
muhammed erdem <erdem.muhammed1@outlook.com>
2021-09-25 20:39:21 +0200

    const text and color

--- StrongPassword--VueJS/script.js
+++ StrongPassword--VueJS/script.js
@@ -1,1 +1,17 @@
-
+let pass_s = 0;
+const text =
+[
+    "",
+    "Passwort too weak",
+    "Passwort okey",
+    "Passwort strong",
+    "Passwort too strong"
+]
+const color =
+[
+    "rgb(197, 2, 2)",
+    "rgb(197, 2, 2)",
+    "rgb(197, 145, 2)",
+    "rgb(74, 197, 2)",
+    "rgb(2, 109, 197)"
+]
