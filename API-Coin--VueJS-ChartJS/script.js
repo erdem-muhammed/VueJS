c3877b7d14c8474dc7a47b6023b1b5a2103200a1
muhammed erdem <erdem.muhammed1@outlook.com>
2021-10-12 22:53:06 +0200

    btnClick funtion added to button

--- API-Coin--VueJS-ChartJS/script.js
+++ API-Coin--VueJS-ChartJS/script.js
@@ -46,5 +46,36 @@ const app = new Vue({
         .then(json => answer(json))
     },
     methods:
-    {}
+    {
+        btnClick(index)
+        {
+            let id = this.buttons[index].id;
+
+            fetch("https://coinranking1.p.rapidapi.com/coin/" + id + "/history/1y", //1y = one year
+            {
+                "method": "GET",
+                "headers": {
+                    "x-rapidapi-host": "coinranking1.p.rapidapi.com",
+                    "x-rapidapi-key": "f464c0068amsh83cba0ecdfd1549p1171b8jsn78f7366baa2b"
+                }
+            })
+            .then(answer => antwort.json())
+            .then(function(json)
+            {
+                graphicData.datasets[0].label = app.buttons[index].name + "-Currency";
+
+                for(let i = 0; i < json.data.history.length; i += 30)
+                {
+                    const date = new Date(json.data.history[i].timestamp)
+                    labels.push(date.getDate() + "." + (date.getMonth()+1) + "." + date.getFullYear());
+                    grafikData.datasets[0].data.push(json.data.history[i].price);
+                }
+
+                new Chart(
+                    document.getElementById("myChart"),
+                    config
+                )
+            })
+        }
+    }
 })
