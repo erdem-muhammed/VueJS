de69b64ed92c83305bd21a787c0c745231ea8398
muhammed erdem <erdem.muhammed1@outlook.com>
2021-10-12 22:42:18 +0200

    created() has been made and fetch() dded to new Vue

--- API-Coin--VueJS-ChartJS/script.js
+++ API-Coin--VueJS-ChartJS/script.js
@@ -15,5 +15,19 @@ const app = new Vue({
     data:
     {
         buttons[],
-    }
+        result: ""
+    },
+    created()
+    {
+        fetch("https://coinranking1.p.rapidapi.com/coins",
+        {
+            "method": "GET",
+            "headers": {
+                "x-rapidapi-host": "coinranking1.p.rapidapi.com",
+                "x-rapidapi-key": "f464c0068amsh83cba0ecdfd1549p1171b8jsn78f7366baa2b"
+            }
+        })
+        .then(response => response.json())
+        .then(json => antwortGekommen(json))
+    },
 })
