b3710161809cf81815ea298c9b2aab771d6ef858
muhammed erdem <erdem.muhammed1@outlook.com>
2021-11-20 19:42:38 +0100

    const users added

---
+++ Admin-Login-Dashboard/script.js
@@ -0,0 +1,81 @@
+const users =
+[
+    {
+        id: 0,
+        vorname: "Max",
+        nachname: "Mustermann",
+		alter: 25
+    },
+    {
+        id: 1,
+        vorname: "Lucas",
+        nachname: "Müller",
+		alter: 38
+    },
+    {
+        id: 2,
+        vorname: "Laura",
+        nachname: "Musterfrau",
+		alter: 30
+    },
+    {
+        id: 3,
+        vorname: "Emma",
+        nachname: "Jäger",
+		alter: 26
+    },
+    {
+        id: 4,
+        vorname: "Anton",
+        nachname: "Schneider",
+		alter: 40
+    },
+    {
+        id: 5,
+        vorname: "Martin",
+        nachname: "Schmidt",
+		alter: 38
+    },
+    {
+        id: 6,
+        vorname: "Jena",
+        nachname: "Fischer",
+		alter: 21
+    },
+    {
+        id: 7,
+        vorname: "Alex",
+        nachname: "Weber",
+		alter: 34
+    },
+    {
+        id: 8,
+        vorname: "Karl",
+        nachname: "Meyer",
+		alter: 51
+    },
+    {
+        id: 9,
+        vorname: "Marcus",
+        nachname: "Wagner",
+		alter: 29
+    },
+    {
+        id: 10,
+        vorname: "Helen",
+        nachname: "Becker",
+		alter: 22
+    },
+    {
+        id: 11,
+        vorname: "Lina",
+        nachname: "Schulz",
+		alter: 19
+    },
+    {
+        id: 12,
+        vorname: "Peter",
+        nachname: "Hoffmann",
+		alter: 42
+    },
+]
